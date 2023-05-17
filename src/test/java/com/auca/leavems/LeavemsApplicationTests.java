3662d96284999737536ce8bdaad62411004e5ffc
Aimeana100 <aimeanathole@gmail.com>
2023-05-17 09:57:13 +0200

    Leave management system first commit

---
+++ src/test/java/com/auca/leavems/LeavemsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auca.leavems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeavemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
